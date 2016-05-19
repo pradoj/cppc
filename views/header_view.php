311b8b566defa13152c40505311c6274ef5d3eff
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2016-05-19 12:04:40 -0300

    Move all to html/static only

--- views/header_view.php
+++
@@ -1,5 +0,0 @@
-<div data-role="header">
-    <!--<h1><img src="logo_200x100.png" width="200" height="100" alt="Logo" /></h1>-->
-    <h2><a href="<?php echo BASE_URL; ?>">Calculadora percentual CPPC</a></h2>
-    <h3><a href="http://pradoj.com">pradoj.com</a></h3>
-</div>
